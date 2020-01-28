66f2d82d8402f0625d9e67261f871c5c2028cd46
Adriano Systax <adriano@systax.com.br>
2020-01-28 17:45:34 -0300

    ajustando call api cotacao

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,69 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/b3', function(req, res, next) {
-  console.log('b3 passando')
+  // console.log('b3 passando');
+
+  // ---------------
   var unirest = require('unirest');
-  var content = 'b';
+  // console.error('1.');
 
   var req = unirest('GET', 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&apikey=NP75EV2DGLZWIGHM&symbol=LINX3.SA')
     .end(function (resp) { 
-      if (resp.error) throw new Error(resp.error); 
-      console.log(content);
-      content = resp.raw_body;
-      console.log(content);
-      // console.log(resp.raw_body);
+      // console.error(resp);
+
+      // if (resp.error) throw new Error(resp.error); 
+
+      if (!resp.error && resp.status == 200) {
+        res.send(resp.body);
+      }
     });
-    
-  
-    res.send(content);
+  // ---------------
 });
 
 module.exports = router;
+
+
+/*
+
+
+
+
+    
+  // var https = require('follow-redirects').https;
+  const { http, https } = require('follow-redirects');
+  const fs = require('fs');
+
+  var options = {
+    'method': 'GET',
+    'hostname': 'www.alphavantage.co',
+    'path': '/query?function=GLOBAL_QUOTE&apikey=NP75EV2DGLZWIGHM&symbol=WEGE3.SA',
+    'headers': {
+    },
+    'maxRedirects': 20
+  };
+
+  var req = https.request(options, function (res) {
+    var chunks = [];
+
+    res.on("data", function (chunk) {
+      console.error('1.');
+      chunks.push(chunk);
+    });
+
+    res.on("end", function (chunk) {
+      console.error('2.');
+      var body = Buffer.concat(chunks);
+      console.log(body.toString());
+      console.error('3.');
+    });
+
+    res.on("error", function (error) {
+      console.error('4.');
+      console.error(error);
+    });
+  });
+
+  req.end(() => {
+    console.error('6.');
+  });
+*/

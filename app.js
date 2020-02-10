e78e52966175dcc2a5425b1951b3eec24a8022bd
Adriano Systax <adriano@systax.com.br>
2020-02-10 14:57:55 -0300

    add helmet

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var priceRouter = require('./routes/price');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const priceRouter = require('./routes/price');
 
-var app = express();
+const app = express();
+
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
